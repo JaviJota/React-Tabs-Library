53efa17987da2f9018461a050c96dcfc67de1a48
Javier Jiménez <javierjimenezcarralero@gmail.com>
2025-01-10 15:22:18 +0100

    First commit

---
+++ src/components/ColumnUnderlinedTabs.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { TabsProps } from "../types/tabs";
+
+const ColumnUnderlinedTabs: React.FC<TabsProps> = ({
+  tabs,
+  tabContent,
+  bgColor,
+  textColor,
+}) => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  return (
+    <div>
+      <div className="flex flex-col space-x-1 mt-11 items-center">
+        {tabs?.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`${
+              activeTab === tab.id ? "" : "hover:opacity-50"
+            } relative rounded-full px-3 py-1.5 text-sm font-medium ${
+              textColor || "text-white"
+            }
+          outline-sky-400 focus-visible:outline-2`}
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {tab.label}
+            {activeTab === tab.id && (
+              <motion.div
+                layoutId="active-tab"
+                className={`absolute h-1 top-7 inset-0 z-10 ${
+                  bgColor || "bg-white"
+                } mix-blend-difference`}
+                style={{
+                  borderRadius: 9999,
+                }}
+                transition={{ type: "spring", duration: 0.6, bounce: 0.2 }}
+              />
+            )}
+          </button>
+        ))}
+      </div>
+      <div className="text-center mt-10 text-5xl font-medium">
+        {tabContent[activeTab]}
+      </div>
+    </div>
+  );
+};
+
+export default ColumnUnderlinedTabs;
---
+++ src/components/PillTabs.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { TabsProps } from "../types/tabs";
+
+const PillTabs: React.FC<TabsProps> = ({
+  tabs,
+  tabContent,
+  bgColor,
+  textColor,
+}) => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  return (
+    <div>
+      <div className="flex flex-wrap space-x-1 mt-11 justify-center">
+        {tabs?.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`${
+              activeTab === tab.id ? "" : "hover:opacity-50"
+            } relative rounded-full px-3 py-1.5 text-sm font-medium ${
+              textColor || "text-white"
+            }
+          outline-sky-400 focus-visible:outline-2`}
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {activeTab === tab.id && (
+              <motion.div
+                layoutId="active-tab"
+                className={`absolute inset-0 z-10 ${
+                  bgColor || "bg-white"
+                } mix-blend-difference`}
+                style={{
+                  borderRadius: 9999,
+                }}
+                transition={{ type: "spring", duration: 0.6, bounce: 0.2 }}
+              />
+            )}
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className="text-center mt-10 text-5xl font-medium">
+        {tabContent[activeTab]}
+      </div>
+    </div>
+  );
+};
+
+export default PillTabs;
